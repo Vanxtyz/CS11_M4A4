8570a62d0aad368d9ee95196103b8f5e8f4d123b
Vanxtyz <jo.vcordoba10@gmail.com>
2025-11-20 20:30:15 +0000

    added radius and expression solver

--- expression.py
+++ expression.py
@@ -1,1 +1,11 @@
+# Expression Solver Program
 
+def solve_expression(a, b):
+    return (a - b) + (a * b)
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+result = solve_expression(a, b)
+
+print("The result of (a - b) + (a * b) is: " + str(result))
--- radius.py
+++ radius.py
@@ -1,1 +1,5 @@
-
+radius_str = input("Enter the radius of the circle: ")
+radius = int(radius_str)
+area = 3.14159 * radius * radius
+area_str = str(area)
+print("The area of the circle with radius " + radius_str + " is " + area_str)
